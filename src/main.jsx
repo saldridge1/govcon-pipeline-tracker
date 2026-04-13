fa62efe6b508bf988de13d9b5cc87567bbadae29
Susan Aldridge <susanaldridge555@icloud.com>
2026-04-13 14:58:00 -0500

    Update main.jsx to load GovCon Ready v2.0

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import GovConPipeline from './GovConPipelineTracker'
+import GovConReady from './GovConReady'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <GovConPipeline />
+    <GovConReady />
   </React.StrictMode>
 )
